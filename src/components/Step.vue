4aafe519f9070ef020920af24e718d4579b7dc68
Trung Vo <trunglive@gmail.com>
2021-01-17 23:04:27 +0700

    added initial layout for step Create Email

--- src/components/Step.vue
+++ src/components/Step.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-    hello
+    <div class="min-h-screen flex flex-col items-center justify-center">
+      <div class="flex flex-col items-center mb-10">
+        <div class="flex flex-col items-center justify-center w-16 h-16 rounded-md bg-blue-light font-bold text-2xl">S</div>
+        <div class="text-black-dark font-bold text-xl mt-6">Create Account</div>
+      </div>
+      <div>
+        <input id="email" name="email" type="email" autocomplete="off" required class="appearance-none rounded-none w-full py-2 placeholder-gray-500 text-gray-900 focus:outline-none focus:z-10" placeholder="Please input email">
+        <div class="border-t border-dashed">
+          <div class="flex flex-col mt-4">
+            <a href="#" class="px-36 py-3 rounded-md text-white font-bold bg-ocean hover:bg-ocean-dark duration-100 ease-in-out">
+              Continue
+            </a>
+            <a href="#" class="text-black mt-3.5 hover:text-black-light duration-100 ease-in-out">
+              Back
+            </a>
+          </div>
+        </div>
+      </div>
+
   </div>
 </template>
 
@@ -15,18 +32,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ocean: {
+        // light: '',
+        DEFAULT: '#059fa3',
+        dark: '#1d5f75',
+      },
+      blue: {
+        light: '#a7c5eb',
+        DEFAULT: '#51adcf',
+        // dark: '',
+      },
+      black: {
+        light: '#747474',
+        DEFAULT: '#40474f',
+        dark: '#212528',
+      },
+      white: colors.white,
+    },
     extend: {},
   },
   variants: {
