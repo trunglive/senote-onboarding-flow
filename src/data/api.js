cb251871cefc9db55c273de5324fba085feeb62b
Trung Vo <trunglive@gmail.com>
2021-01-24 14:30:05 +0700

    added more data for step Analyze & Prototype

--- src/data/api.js
+++ src/data/api.js
@@ -40,10 +40,50 @@ export const designThinkingProcesses = {
     label: "Analyze",
     goal: "Finding patterns",
     phaseStepOrder: 2,
+    stepProcesses: [
+      {
+        label: "Problem Valuation",
+        value: "problemValuation",
+        tooltipContent:
+          "Examine your data and evaluate all the problems you discovered so far from your interviews.",
+      },
+      {
+        label: "Personas",
+        value: "personas",
+        tooltipContent:
+          "Streamline your research data and represent the specific considerations for fundamentally different groups of people.",
+      },
+      {
+        label: "Solution Valuation",
+        value: "solutionValuation",
+        tooltipContent:
+          "Tackle the most crucial problems by brainstorming different solutions. Come up with as many solutions as possible.",
+      },
+      {
+        label: "Flows",
+        value: "flows",
+        tooltipContent:
+          "Connect the dots between the identified problems and their solutions. List down 5-8 steps showing a clear path from the problem to its corresponding solution.",
+      },
+    ],
   },
   prototype: {
     label: "Prototype",
     goal: "Visualize findings",
     phaseStepOrder: 3,
+    stepProcesses: [
+      {
+        label: "Paper Prototype",
+        value: "paperPrototype",
+        tooltipContent:
+          "Start with simple prototypes created on paper. It's a simple and cheap way to test your product without much effort.",
+      },
+      {
+        label: "Interactive Prototype",
+        value: "interactivePrototype",
+        tooltipContent:
+          "Use a hi-fidelity prototype to gain more precise feedback and take your prototype to the next level.",
+      },
+    ],
   },
 };
