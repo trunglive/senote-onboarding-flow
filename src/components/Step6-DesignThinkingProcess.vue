41b164426965b4ef2141ffbc26d0db45afad0af0
Trung Vo <trunglive@gmail.com>
2021-01-24 12:39:11 +0700

    added data & ui for design thinking process

---
+++ src/components/Step6-DesignThinkingProcess.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center">
+    <div class="flex flex-col items-center w-80 mb-2">
+      <div class="text-black-dark text-xl mt-6">
+        {{ brand.label }} is equipped with predefined design thinking processes
+        divided into three phrases.
+      </div>
+      <div>
+        <div>Discover</div>
+        <div>Analyze</div>
+        <div>Prototype</div>
+      </div>
+    </div>
+    <NavigationButtonGroup :send="send" />
+  </div>
+</template>
+
+<script>
+import NavigationButtonGroup from "@/components/NavigationButtonGroup";
+import { brand } from "@/data/api";
+
+export default {
+  name: "DesignThinkingProcess",
+  props: {
+    send: Function,
+  },
+  components: {
+    NavigationButtonGroup,
+  },
+  setup() {
+    return {
+      brand,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
--- src/data/api.js
+++ src/data/api.js
@@ -9,3 +9,41 @@ export const userAddPassword = {
 export const brand = {
   label: "Senote",
 };
+
+export const designThinkingProcesses = {
+  discover: {
+    label: "Discover",
+    goal: "Gathering data",
+    phaseStepOrder: 1,
+    stepProcesses: [
+      {
+        label: "Stakeholder Interview",
+        value: "stakeholderInterview",
+        tooltipContent:
+          "Gain a deeper understanding of client's expectations. Start with predefined questions to ask your stakeholder.",
+      },
+      {
+        label: "User Interview",
+        value: "userInterview",
+        tooltipContent:
+          "Recognize the problems and paint points of your target audience while talking to them.",
+      },
+      {
+        label: "Competitor Analysis",
+        value: "competitorAnalysis",
+        tooltipContent:
+          "Analyze your competitors and evaluate their strengths, weaknesses and distinguishing features.",
+      },
+    ],
+  },
+  analyze: {
+    label: "Analyze",
+    goal: "Finding patterns",
+    phaseStepOrder: 2,
+  },
+  prototype: {
+    label: "Prototype",
+    goal: "Visualize findings",
+    phaseStepOrder: 3,
+  },
+};
