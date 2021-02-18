c3294bd775face96e6fed1818a15dfae1123efe5
Trung Vo <trunglive@gmail.com>
2021-02-18 23:23:15 +0700

    fixed validation for password

--- src/components/AddPassword.vue
+++ src/components/AddPassword.vue
@@ -7,7 +7,7 @@
     <div>
       <div class="flex items-center justify-between w-navigation-button">
         <input
-          v-model="passwordInput"
+          v-model="state.password"
           id="password"
           name="password"
           type="password"
@@ -16,28 +16,46 @@
           class="appearance-none rounded-none py-2 placeholder-gray-500 text-gray-900 focus:outline-none focus:z-10"
           placeholder="Enter your password"
         >
-        <Checkmark />
+        <Checkmark v-if="!v$.password.$invalid" />
       </div>
       <div class="border-t border-dashed">
         <div class="mt-4 text-sm text-left">
-          <span>Password should be at least</span>
-          <span class="font-bold"> 8 characters long</span>
+          <span>Password should be at least </span>
+          <span class="font-bold"> {{ passwordMinLength }} characters long</span>
         </div>
-        <NavigationButtonGroup :send="send" />
+        <NavigationButtonGroup
+          :send="send"
+          :disable-continue-button="v$.password.$invalid"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { reactive } from "vue"
 import NavigationButtonGroup from "@/components/NavigationButtonGroup"
 import AvatarGroup from "@/components/AvatarGroup"
 import Checkmark from "@/components/icons/Checkmark"
+import { minLength, required } from "@vuelidate/validators"
+import { useVuelidate } from '@vuelidate/core'
+
+const passwordMinLength = 8
 
 export default {
 	name: "AddPassword",
 	props: {
 		send: Function
+	},
+	setup() {
+		const state = reactive({
+			password: ''
+		})
+		const rules = {
+			password: { required, minLength: minLength(passwordMinLength) }
+		}
+		const v$ = useVuelidate(rules, state)
+		return { state, v$, passwordMinLength }
 	},
 	components: { AvatarGroup, NavigationButtonGroup, Checkmark }
 }
