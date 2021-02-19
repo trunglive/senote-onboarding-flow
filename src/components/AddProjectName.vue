e05d9d6a3646211f9c51820d8760d8ff0ffbff26
Trung Vo <trunglive@gmail.com>
2021-02-19 10:48:18 +0700

    fixed validation for project name

--- src/components/AddProjectName.vue
+++ src/components/AddProjectName.vue
@@ -5,26 +5,32 @@
     </div>
     <div>
       <input
-        v-model="projectNameInput"
-        id="email"
-        name="email"
-        type="email"
+        v-model="state.projectName"
+        id="projectName"
+        name="projectName"
+        type="text"
         autocomplete="off"
         required
         class="appearance-none rounded-none w-navigation-button py-2 placeholder-gray-500 text-center text-black-light focus:outline-none focus:z-10"
         placeholder="Research Project"
       >
       <div class="border-t border-dashed">
-        <NavigationButtonGroup :send="send" />
+        <NavigationButtonGroup
+          :send="send"
+          :disable-continue-button="v$.projectName.$invalid"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { reactive } from "vue"
 import NavigationButtonGroup from "@/components/NavigationButtonGroup"
 import { brand } from "@/data/api"
 import StepInfo from "@/components/StepIntro"
+import { required } from "@vuelidate/validators"
+import useVuelidate from "@vuelidate/core"
 
 export default {
 	name: "AddProjectName",
@@ -36,11 +42,23 @@ export default {
 		NavigationButtonGroup
 	},
 	setup(props) {
+		const state = reactive({
+			projectName: ''
+		})
+		const rules = {
+			projectName: {
+				required
+			}
+		}
+		const v$ = useVuelidate(rules, state)
+
 		function composeStepTitle() {
 			return `What's the name of the project you want to use on ${brand.label}?`
 		}
 		return {
 			composeStepTitle,
+			state,
+			v$,
 		}
 	}
 }
