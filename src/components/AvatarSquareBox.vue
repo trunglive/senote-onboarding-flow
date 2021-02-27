07cff605699634a2ff525e63d8193331cf4d9c97
Trung Vo <trunglive@gmail.com>
2021-02-27 20:36:58 +0700

    added custom inner circle for unknown avatar

--- src/components/AvatarSquareBox.vue
+++ src/components/AvatarSquareBox.vue
@@ -1,7 +1,7 @@
 <template>
   <div
-    :class="[customSize]"
-    class="flex flex-col items-center justify-center rounded-md bg-white-dark font-bold text-2xl"
+    :class="[customSize, customBackgroundColor, enableEmptyAvatar && 'avatar--inner-circle']"
+    class="flex flex-col items-center justify-center rounded-md font-bold text-2xl relative"
   >
     <div
       v-if="avatarLetter?.length > 0"
@@ -22,15 +22,33 @@ export default {
       type: String,
       default: 'w-16 h-16'
     },
+    customBackgroundColor: {
+      type: String,
+      default: 'bg-white-dark',
+    },
     avatarLetter: {
       type: String,
       default: null,
-    }
+    },
+    enableEmptyAvatar: {
+      type: Boolean,
+      default: false,
+    },
   },
   components: { Mailbox },
 }
 </script>
 
 <style scoped>
-
+.avatar--inner-circle:before {
+  content: "";
+  width: 32px;
+  height: 32px;
+  background-color: #edf0f2;
+  border-radius: 50%;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
--- src/components/analyze/Personas.vue
+++ src/components/analyze/Personas.vue
@@ -1,17 +1,28 @@
 <template>
-  <div>
+  <div class="flex flex-col items-center justify-center">
     <AvatarSquareBox
-      custom-size="w-12 h-12"
-      avatar-letter="K"
+      custom-size="w-14 h-14"
+      custom-background-color="bg-white-light"
+      :enable-empty-avatar="true"
     />
+    <div class="pt-20">
+      <NavigationButtonGroup
+        :send="send"
+        :disable-continue-button="false"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import AvatarSquareBox from "@/components/AvatarSquareBox"
+import NavigationButtonGroup from "@/components/NavigationButtonGroup"
 export default {
   name: "Personas",
-  components: { AvatarSquareBox }
+  props: {
+    send: Function,
+  },
+  components: { NavigationButtonGroup, AvatarSquareBox }
 }
 </script>
 
